f8d54df0ea309ff7cdecbcdc409271668919bf0b
bbtemb <bbtemb@gmail.com>
2024-02-15 02:44:10 +0500

    Добавил усложненное задание

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 
 const body = document.querySelector('body');
 
+let x = 0;
+let y = 0;
+
 function DomElement(selector) {
   this.selector = selector;
   this.height = '100px';
-  this.width = '200px';
+  this.width = '100px';
   this.bg = 'pink';
   this.fontSize = '16px';
+  this.position = 'absolute';
 
   this.createElement = function () {
     let newEl;
@@ -22,11 +26,31 @@ function DomElement(selector) {
       newEl.innerText = 'Это p';
       body.append(newEl);
     }
-    newEl.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
+    newEl.classList.add(this.selector.slice(1));
+    newEl.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}; position: ${this.position};`;
   };
 }
-let testBlock = new DomElement('.block');
-testBlock.createElement();
+let square = new DomElement('.block');
 
-let testP = new DomElement('#text');
-testP.createElement();
+document.addEventListener('DOMContentLoaded', square.createElement());
+
+const squareLoaded = document.querySelector('.block');
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'ArrowRight') {
+    x += 10;
+    squareLoaded.style.left = `${x}px`;
+  }
+  if (event.key === 'ArrowLeft') {
+    x -= 10;
+    squareLoaded.style.left = `${x}px`;
+  }
+  if (event.key === 'ArrowUp') {
+    y -= 10;
+    squareLoaded.style.top = `${y}px`;
+  }
+  if (event.key === 'ArrowDown') {
+    y += 10;
+    squareLoaded.style.top = `${y}px`;
+  }
+});
