d28b40642dd9315d94db918ed946a4476d3297ef
bbtemb <bbtemb@gmail.com>
2024-02-17 14:46:40 +0500

    Внёс правки

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const body = document.querySelector('body');
 let x = 0;
 let y = 0;
 
-function DomElement(selector) {
+function DomElement(selector, height, width, bg, fontSize) {
   this.selector = selector;
-  this.height = '100px';
-  this.width = '100px';
-  this.bg = 'pink';
-  this.fontSize = '16px';
+  this.height = height || '100px';
+  this.width = width || '100px';
+  this.bg = bg || 'pink';
+  this.fontSize = fontSize || '20px';
   this.position = 'absolute';
 
   this.createElement = function () {
@@ -18,19 +18,21 @@ function DomElement(selector) {
     if (selector.charAt(0) === '.') {
       newEl = document.createElement('div');
       newEl.innerText = 'Это div';
+      newEl.classList.add(this.selector.slice(1));
       body.append(newEl);
     }
 
     if (selector.charAt(0) === '#') {
       newEl = document.createElement('p');
       newEl.innerText = 'Это p';
+      newEl.setAttribute('id', this.selector.slice(1));
       body.append(newEl);
     }
-    newEl.classList.add(this.selector.slice(1));
+
     newEl.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}; position: ${this.position};`;
   };
 }
-let square = new DomElement('.block');
+let square = new DomElement('.block', '500px', '500px', 'orange', '20px');
 
 document.addEventListener('DOMContentLoaded', square.createElement());
 
